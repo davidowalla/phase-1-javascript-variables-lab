4a753d050a58f8ab98bbdabd2dd1b33a7a0aaf91
david@thekayappo.com <davidowalla@173-10-0-241-Delmarva.hfc.comcastbusiness.net>
2023-10-24 11:23:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+
+const companyName = "Scuber";
+let employeeName = "John";
+let employeeAge = "30";
+
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Susan Smith"; 
